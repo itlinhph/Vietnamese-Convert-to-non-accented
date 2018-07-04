abab2cfd33397d488796c638376b4866875e1839
linkpp <linhphanhust@gmail.com>
2018-07-04 16:19:46 +0700

    add xoadau in python

---
+++ xoadau.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import re
+
+patterns = {
+    '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+    '[đ]': 'd',
+    '[èéẻẽẹêềếểễệ]': 'e',
+    '[ìíỉĩị]': 'i',
+    '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+    '[ùúủũụưừứửữự]': 'u',
+    '[ỳýỷỹỵ]': 'y'
+}
+
+def convert(text):
+  
+    output = text
+    for regex, replace in patterns.items():
+        output = re.sub(regex, replace, output)
+        # deal with upper case
+        output = re.sub(regex.upper(), replace.upper(), output)
+    return output
+
+if __name__ == '__main__':
+    address = "Sô Nha ̀ 223-khu phố 1 phường phước bình Thị xã Phước Long, Bình Phước"
+    print(convert(address))
