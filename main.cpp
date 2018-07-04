59b6befa52a7873970da0844fe44a7b661cc1ae8
linkpp <linhphanhust@gmail.com>
2018-07-04 16:22:27 +0700

    add replaceAll in c++

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@
 #include <iterator>
 using namespace std ;
 
+void replaceAll(std::string& source, const std::string& from, const std::string& to)
+{
+    std::string newString;
+    newString.reserve(source.length());  // avoids a few memory allocations
+
+    std::string::size_type lastPos = 0;
+    std::string::size_type findPos;
+
+    while(std::string::npos != (findPos = source.find(from, lastPos)))
+    {
+        newString.append(source, lastPos, findPos - lastPos);
+        newString += to;
+        lastPos = findPos + from.length();
+    }
+
+    // Care for the rest after last occurrence
+    newString += source.substr(lastPos);
+
+    source.swap(newString);
+}
+
 int main ()
 {
 
